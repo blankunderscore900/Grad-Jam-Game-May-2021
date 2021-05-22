7e8f0439dddd863eb78efc492da8755299d82387
blankunderscore900 <nicholas_bollong@outlook.com>
2021-05-21 22:57:27 -0300

    Player build
    Player keeps falling through the ground

---
+++ Grad Jam Game May 2021/Assets/Scripts/Player.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+
+    public float speed;
+
+    Rigidbody2D rb;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void FixedUpdate()
+    {
+        float move = Input.GetAxisRaw("Horizontal");
+
+        rb.velocity = new Vector2(speed * move, rb.velocity.y);
+    }
+}
